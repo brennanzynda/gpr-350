76c71c262ca6b241842bfe9e7298f187a79f6e29
Brennan Zynda <brennan.zynda@mymail.champlain.edu>
2019-09-13 10:01:47 -0400

    Initial commit for lab 2, decent amount of progress made on force generator equations

---
+++ Lab2/Assets/Scripts/Physics/ForceGenerator.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lab2ForceGenerator
+{
+    // Lab 2 Step 3
+    public static Vector2 GenerateForce_Gravity(float particleMass, float gravitationalConstant, Vector2 worldUp)
+    {
+        // f_gravity: f = mg
+        Vector2 f_gravity = particleMass * gravitationalConstant * worldUp;
+        return f_gravity;
+    }
+    
+    public static Vector2 GenerateForce_normal(Vector2 f_gravity, Vector2 surfaceNormal_unit)
+    {
+        // f_normal = proj(f_gravity, surfaceNormal_unit)
+        Vector2 f_normal = new Vector2(0, 0);
+        return new Vector2(0,0);
+    }
+    
+    public static Vector2 GenerateForce_sliding(Vector2 f_gravity, Vector2 f_normal)
+    {
+        // f_sliding = f_gravity + f_normal
+        Vector2 f_sliding = f_gravity + f_normal;
+        return f_sliding;
+    }
+    
+    Vector2 GenerateForce_friction_static(Vector2 f_normal, Vector2 f_opposing, float frictionCoefficient_static)
+    {
+        // f_friction_s = -f_opposing if less than max, else -coeff*f_normal (max amount is coeff*|f_normal|)
+        return new Vector2(0, 0);
+    }
+    
+    Vector2 GenerateForce_friction_kinetic(Vector2 f_normal, Vector2 particleVelocity, float frictionCoefficient_kinetic)
+    {
+        // f_friction_k = -coeff*|f_normal| * unit(vel)
+        return new Vector2(0, 0);
+    }
+    
+    Vector2 GenerateForce_drag(Vector2 particleVelocity, Vector2 fluidVelocity, float fluidDensity, float objectArea_crossSection, float objectDragCoefficient)
+    {
+        // f_drag = (p * u^2 * area * coeff)/2
+        Vector2 f_drag = (particleVelocity * fluidVelocity * fluidVelocity * objectArea_crossSection * objectDragCoefficient) / 2;
+        return new Vector2(0, 0);
+    }
+    Vector2 GenerateForce_spring(Vector2 particlePosition, Vector2 anchorPosition, float springRestingLength, float springStiffnessCoefficient)
+    {
+        // f_spring = -coeff*(spring length - spring resting length)
+        return new Vector2(0, 0);
+    }
+}
---
+++ Lab2/Assets/Scripts/Physics/Particle2D.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particle2D : MonoBehaviour
+{
+    // Lab 1 Step 1
+    public Vector2 position, velocity, acceleration;
+    public float rotation, angularVelocity, angularAcceleration;
+    public bool positionUpdateEuler, positionUpdateKinematic;
+    public bool rotationUpdateEuler, rotationUpdateKinematic ;
+
+    // Lab 2 Step 1
+    public float startingMass = 1.0f;
+    public float mass;
+    private float massInv;
+    public void SetMass(float newMass)
+    {
+        // If newMass > 0 then newMass, else 0.0f
+        // mass = newMass > 0.0f ? newMass : 0.0f;
+        mass = Mathf.Max(0.0f, newMass);
+        massInv = mass > 0.0f ? 1.0f / mass : 0.0f;
+    }
+
+    // Lab 2 Step 2
+    Vector2 force;
+    public void AddForce(Vector2 newForce)
+    {
+        // D'Alembert
+        force += newForce;
+    }
+
+    void UpdateAcceleration()
+    {
+        // Newton 2
+        acceleration = force * massInv;
+        force.Set(0.0f, 0.0f);
+    }
+
+    public float GetMass()
+    {
+        return mass;
+    }
+
+    // Lab 1 Step 2
+    void updatePositionEulerExplicit(float dt)
+    {
+        // x(t+dt) = x(t) + v(t)dt
+        // Euler's method:
+        // F(t+dt) = F(t) + f(t)dt
+        //                + (dF/dt)dt
+        position += velocity * dt;
+
+        // v(t+dt) = v(t) + a(t)dt
+        velocity += acceleration * dt;
+    }
+
+    void updatePositionKinematic(float dt)
+    {
+        // x(t+dt) = x(t) + v(t)dt + (a(t)dt^2)/2
+        position += velocity * dt + (acceleration * dt * dt) / 2;
+        velocity += acceleration * dt;
+    }
+
+    void updateRotationEulerExplicit(float dt)
+    {
+        rotation += angularVelocity * dt;
+        angularVelocity += angularAcceleration * dt;
+    }
+
+    void updateRotationKinematic(float dt)
+    {
+        rotation += angularVelocity * dt + (angularAcceleration * dt * dt) / 2;
+        angularVelocity += angularAcceleration * dt;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ResetUnit()
+    {
+        transform.position = new Vector3(0, 0, 0);
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+        angularAcceleration = 0;
+        //angularVelocity = 0;
+        acceleration = new Vector2(0, 0);
+        position = new Vector2(0, 0);
+        //velocity = new Vector2(0, 0);
+        rotation = 0;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Lab 1 Step 3
+        if (positionUpdateEuler)
+        {
+            updatePositionEulerExplicit(Time.fixedDeltaTime);
+            transform.position = position;
+        }
+        else if(positionUpdateKinematic)
+        {
+            updatePositionKinematic(Time.fixedDeltaTime);
+            transform.position = position;
+        }
+
+        else if (rotationUpdateEuler)
+        {
+            updateRotationEulerExplicit(Time.fixedDeltaTime);
+            transform.rotation = Quaternion.Euler(0, 0, rotation);
+        }
+        else if(rotationUpdateKinematic)
+        {
+            updateRotationKinematic(Time.fixedDeltaTime);
+            transform.rotation = Quaternion.Euler(0,0,rotation);
+        }
+
+        // Lab 2 Step 4
+        UpdateAcceleration();
+        // f_gravity: f = m * g; (g = -9.8)
+        //Vector2 f_gravity = mass * new Vector2(0.0f, -9.8f);
+        //AddForce(f_gravity);
+        AddForce(ForceGenerator.GenerateForce_Gravity(mass, -9.8f, Vector2.up));
+        // Lab 1 Step 4
+        //acceleration.x = -Mathf.Sin(Time.time);
+        //angularVelocity += 2;
+    }
+}
